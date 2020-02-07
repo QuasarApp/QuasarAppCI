ae0272b17ffc885cef0f59cd7bf4526bee7815b7
EndrII <endriimail@gmail.com>
2020-02-08 00:17:42 +0300

    try add webhook

--- BuildBotLib/buildBotChangeSource.py
+++ BuildBotLib/buildBotChangeSource.py
@@ -2,51 +2,61 @@
 from BuildBotLib.buildBotModule import BuildBotModule
 from buildbot.plugins import changes
 import os
+from BuildBotLib.secretManager import SecretManager
 
 
 class BuildBotChangeSource(BuildBotModule):
     def __init__(self):
         BuildBotModule.__init__(self)
 
-        self.masterConf['change_source'] = [
-            changes.GitPoller(
-                repourl='git@github.com:QuasarApp/Snake.git',
-                project='qmake-Snake',
-                gitbin=os.path.dirname(os.path.realpath(__file__))
-                + "/git/gitBin.sh",
-                branches=True,  # получаем изменения со всех веток
-                pollInterval=60
-            ),
-            changes.GitPoller(
-                repourl='git@github.com:QuasarApp/CQtDeployer.git',
-                project='qmake-CQtDeployer',
-                gitbin=os.path.dirname(os.path.realpath(__file__))
-                + "/git/gitBin.sh",
-                branches=True,  # получаем изменения со всех веток
-                pollInterval=61
-            ),
-            changes.GitPoller(
-                repourl='git@github.com:QuasarApp/Qt-Secret.git',
-                project='qmake-Qt-Secret',
-                gitbin=os.path.dirname(os.path.realpath(__file__))
-                + "/git/gitBin.sh",
-                branches=True,  # получаем изменения со всех веток
-                pollInterval=62
-            ),
-            changes.GitPoller(
-                repourl='git@github.com:QuasarApp/Hanoi-Towers.git',
-                project='qmake-Hanoi-Towers',
-                gitbin=os.path.dirname(os.path.realpath(__file__))
-                + "/git/gitBin.sh",
-                branches=True,  # получаем изменения со всех веток
-                pollInterval=63
-            ),
-            changes.GitPoller(
-                repourl='https://github.com/usermeme/chat-vironit.git',
-                project='npm-Chat',
-                gitbin=os.path.dirname(os.path.realpath(__file__))
-                + "/git/gitBin.sh",
-                branch='master',
-                pollInterval=64
-            )
+        secret = SecretManager("/home/andrei/buildBotSecret/secret.json")
+
+        self.masterConf['www']['change_hook_dialects'] = [
+            { 'github': {
+                'secret': secret.getValue('WebHook'),
+            },
+            },
         ]
+
+#        self.masterConf['change_source'] = [
+#            changes.GitPoller(
+#                repourl='git@github.com:QuasarApp/Snake.git',
+#                project='qmake-Snake',
+#                gitbin=os.path.dirname(os.path.realpath(__file__))
+#                + "/git/gitBin.sh",
+#                branches=True,  # получаем изменения со всех веток
+#                pollInterval=60
+#            ),
+#            changes.GitPoller(
+#                repourl='git@github.com:QuasarApp/CQtDeployer.git',
+#                project='qmake-CQtDeployer',
+#                gitbin=os.path.dirname(os.path.realpath(__file__))
+#                + "/git/gitBin.sh",
+#                branches=True,  # получаем изменения со всех веток
+#                pollInterval=61
+#            ),
+#            changes.GitPoller(
+#                repourl='git@github.com:QuasarApp/Qt-Secret.git',
+#                project='qmake-Qt-Secret',
+#                gitbin=os.path.dirname(os.path.realpath(__file__))
+#                + "/git/gitBin.sh",
+#                branches=True,  # получаем изменения со всех веток
+#                pollInterval=62
+#            ),
+#            changes.GitPoller(
+#                repourl='git@github.com:QuasarApp/Hanoi-Towers.git',
+#                project='qmake-Hanoi-Towers',
+#                gitbin=os.path.dirname(os.path.realpath(__file__))
+#                + "/git/gitBin.sh",
+#                branches=True,  # получаем изменения со всех веток
+#                pollInterval=63
+#            ),
+#            changes.GitPoller(
+#                repourl='https://github.com/usermeme/chat-vironit.git',
+#                project='npm-Chat',
+#                gitbin=os.path.dirname(os.path.realpath(__file__))
+#                + "/git/gitBin.sh",
+#                branch='master',
+#                pollInterval=64
+#            )
+#        ]
