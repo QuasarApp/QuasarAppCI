003f8372877b72d2d01998343a1afa283e7c6082
EndrII <EndrIIMail@gmail.com>
2020-10-19 20:06:49 +0300

    added docs gen

---
+++ BuildBotLib/Docs.py
@@ -0,0 +1,37 @@
+# This Python file uses the following encoding: utf-8
+
+from BuildBotLib.cmake import CMake
+from buildbot.plugins import util, steps
+
+
+class Docs(CMake):
+
+    def __init__(self, platform):
+        CMake.__init__(self, platform)
+
+    def generatePlatformSteps(self, platform):
+
+        platformXcmd = {
+            CMake.P_Linux: self.linuxXmakeCmd,
+            CMake.P_Windows: self.windowsXmakeCmd,
+            CMake.P_Android: self.androidXmakeCmd,
+            CMake.P_Wasm: self.wasmXmakeCmd,
+        }
+
+        res = []
+
+        res += [self.generateStep(platformXcmd[platform],
+                                  platform,
+                                  self.makePrefix() + 'Make',
+                                  lambda step: True)]
+
+        res += [self.generateStep(self.makeTarget('doc'),
+                                  platform,
+                                  'Generate docs for the project',
+                                  self.isDeploy)]
+
+        res += [steps.CopyDirectory(
+                    src="docs/html",
+                    dest=util.Interpolate('%(prop:copyFolder)s'))]
+
+        return res
