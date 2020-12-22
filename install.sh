b59e7df6cac3993720866df234c89de82bf981a1
web <web@web.com>
2020-12-22 12:14:54 +0000

    fix install script

--- install.sh
+++ install.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 service=/etc/systemd/system/buildbot.service
-ROOT=BASE_DIR=$(dirname "$(readlink -f "$0")")
+ROOT=$(dirname "$(readlink -f "$0")")
 
-echo "[Unit]" >> $service
+echo "[Unit]" > $service
 echo "Description=BuildBot" >> $service
 echo "[Service]" >> $service
 echo "ExecStart=$ROOT/start.sh" >> $service
+echo "ExecStop=$ROOT/stop.sh" >> $service
 echo "Type=forking" >> $service
 echo "PIDFile=$ROOT/twistd.pid" >> $service
+echo "WorkingDirectory=$ROOT" >> $service
 echo "[Install]" >> $service
 echo "WantedBy=multi-user.target" >> $service 
 
+systemctl enable buildbot
+systemctl daemon-reload
