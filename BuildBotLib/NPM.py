bae3d674c19c52e96e3692c351e3b03e000bb51b
EndrII <EndrIIMail@gmail.com>
2020-12-22 14:30:07 +0300

    remove all rudiments

--- BuildBotLib/NPM.py
+++
@@ -1,9 +0,0 @@
-# This Python file uses the following encoding: utf-8
-
-from BuildBotLib.basemodule import BaseModule
-
-
-class NPM(BaseModule):
-
-    def __init__(self):
-        BaseModule.__init__(self)
--- BuildBotLib/buildBotChangeSource.py
+++ BuildBotLib/buildBotChangeSource.py
@@ -1,13 +1,14 @@
 # This Python file uses the following encoding: utf-8
 from BuildBotLib.buildBotModule import BuildBotModule
 from BuildBotLib.secretManager import SecretManager
+from pathlib import Path
 
 
 class BuildBotChangeSource(BuildBotModule):
     def __init__(self, masterConf):
         BuildBotModule.__init__(self, masterConf)
 
-        secret = SecretManager("/home/andrei/buildBotSecret/secret.json")
+        secret = SecretManager(str(Path.home()) + "/buildBotSecret/secret.json")
 
         self.masterConf['www']['change_hook_dialects'] = {
                 'github':
