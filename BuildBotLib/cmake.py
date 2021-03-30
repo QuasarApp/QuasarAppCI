231fe3ce7f6b129dee7e00d0d0c63e9b394adde6
EndrII <EndrIIMail@gmail.com>
2021-03-30 20:36:26 +0300

    try fix android o

--- BuildBotLib/cmake.py
+++ BuildBotLib/cmake.py
@@ -42,7 +42,6 @@ class CMake(Make):
         options = [
             'cmake -DCMAKE_PREFIX_PATH=$QTDIR',
             '-DQT_DIR=$QTDIR',
-            '-DQT_QMAKE_EXECUTABLE=%QTDIR%/bin/qmake',
             '-DANDROID_ABI=arm64-v8a',
             '-DANDROID_BUILD_ABI_arm64-v8a=ON',
             '-DANDROID_BUILD_ABI_armeabi-v7a=ON',
