ac47c092733c0211671c5af03e8c65414b7858d2
EndrII <EndrIIMail@gmail.com>
2021-03-30 19:41:10 +0300

    added support android toolchain file of the android

--- BuildBotLib/cmake.py
+++ BuildBotLib/cmake.py
@@ -37,6 +37,7 @@ class CMake(Make):
 
     def androidXmakeCmd(self, props):
         secret = SecretManager(self.home + "/buildBotSecret/secret.json")
+        toochainFile = 'ndk-bundle/build/cmake/android.toolchain.cmake'
 
         options = [
             'cmake -DCMAKE_PREFIX_PATH=$QTDIR',
@@ -44,6 +45,7 @@ class CMake(Make):
             '-DANDROID_ABI=arm64-v8a',
             '-DANDROID_BUILD_ABI_arm64-v8a=ON',
             '-DANDROID_BUILD_ABI_armeabi-v7a=ON',
+            '-DCMAKE_TOOLCHAIN_FILE=$ANDROID_NDK_ROOT/' + toochainFile,
             '-DSIGN_PATH="' + secret.getValue('SIGPATH') + '"',
             '-DSIGN_ALIES="quasarapp"',
             '-DSIGN_STORE_PASSWORD="' + secret.getValue('SIGPASS') + '"',
