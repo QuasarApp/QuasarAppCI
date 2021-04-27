232e2479f5876d48f357984356d22d64e0e3ea8d
EndrII <EndrIIMail@gmail.com>
2021-04-27 16:23:40 +0300

    change native api lvl

--- BuildBotLib/cmake.py
+++ BuildBotLib/cmake.py
@@ -73,7 +73,7 @@ class CMake(Make):
             '-DSIGN_PATH="' + secret.getValue('SIGPATH') + '"',
             '-DSIGN_ALIES="quasarapp"',
             '-DSIGN_STORE_PASSWORD="' + secret.getValue('SIGPASS') + '"',
-            '-DANDROID_PLATFORM_LEVEL=$ANDROID_API_VERSION',
+            '-DANDROID_NATIVE_API_LEVEL=$ANDROID_API_VERSION',
             '-B cmake_build'
         ]
 
@@ -96,7 +96,7 @@ class CMake(Make):
             '-DSIGN_PATH="' + secret.getValue('SIGPATH') + '"',
             '-DSIGN_ALIES="quasarapp"',
             '-DSIGN_STORE_PASSWORD="' + secret.getValue('SIGPASS') + '"',
-            '-DANDROID_PLATFORM_LEVEL=$ANDROID_API_VERSION',
+            '-DANDROID_NATIVE_API_LEVEL=$ANDROID_API_VERSION',
             '-B cmake_build'
         ]
 
